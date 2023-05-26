62e19f3b717110aef57025cce94a0bba32660ac8
Swapneeth Gorantla <swapneeth.gorantla@shopify.com>
2023-05-26 06:48:41 -0400

    Fix setup script error

--- antlr-setup.sh
+++ antlr-setup.sh
@@ -6,11 +6,11 @@ sudo mv antlr4.11.1-complete.jar /usr/local/lib
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo 'export CLASSPATH=".:/usr/local/lib/antlr-4.11.1-complete.jar:${CLASSPATH}"' >> ~/.zprofile
     echo 'alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.11.-complete.jar:${CLASSPATH}" org.antlr.v4.Tool'' >> ~/.zshrc
-    'alias grun='java org.antlr.v4.gui.TestRig'' >> ~/.zshrc
+    echo 'alias grun='java org.antlr.v4.gui.TestRig'' >> ~/.zshrc
     source ~/.zshrc
 else
     echo 'export CLASSPATH=".:/usr/local/lib/antlr-4.11.1-complete.jar:${CLASSPATH}"' >> ~/.profile
     echo 'alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.11.-complete.jar:${CLASSPATH}" org.antlr.v4.Tool'' >> ~/.bashrc
-    'alias grun='java org.antlr.v4.gui.TestRig'' >> ~/.bashrc
+    echo 'alias grun='java org.antlr.v4.gui.TestRig'' >> ~/.bashrc
     source ~/.bashrc
 fi
