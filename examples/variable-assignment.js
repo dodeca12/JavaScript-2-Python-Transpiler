1c937e7fd729a95a25eda687fc34c464334ee569
dodeca12 <dev@sgorantla.ca>
2023-06-25 22:43:17 -0400

    Add examples folder, and a few javascript input files:

---
+++ examples/variable-assignment.js
@@ -0,0 +1,7 @@
+var variable = "supports var keyword";
+let variable2 = "supports let keyword";
+const variable3 = "supports const keyword";
+var numberAssignment = 1;
+let textAssignment = "text";
+const booleanAssignment = true;
+var arrayAssignment = [1, 2, 3];
---
+++ examples/variable-reassignment.js
@@ -0,0 +1,7 @@
+var variable = "supports var keyword";
+variable = "hello"
+variable = 1
+variable = true
+variable = false
+variable = [1, 2, 3]
+variable = []
