baa5e946a455fa4c6823bba526119ba903489d13
dodeca12 <dev@sgorantla.ca>
2023-06-25 23:54:38 -0400

    Add if-statement.js

---
+++ examples/if-statement.js
@@ -0,0 +1,16 @@
+let x = 5;
+if (x < 5){
+    console.log("x is less than 5");
+}else if (x > 5){
+    console.log("x is greater than 5");
+}else {
+    console.log("x is equal to 5");
+}
+var bool = true
+if (bool == false){
+    console.log("bool is false");
+}else if (bool == true){
+    console.log("bool is true"); 
+}else {
+    console.log("bool is neither true nor false");
+}
